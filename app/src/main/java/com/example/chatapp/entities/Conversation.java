f413bf06880a9c083bd318bdc0e7e007fc602de7
Nadav Yakobovich <nadav.yakobovich@gmail.com>
2022-06-06 20:02:46 +0300

    add content and Conversation class

---
+++ app/src/main/java/com/example/chatapp/entities/Conversation.java
@@ -0,0 +1,48 @@
+package com.example.chatapp.entities;
+
+import java.util.List;
+
+public class Conversation {
+    private int Id;
+    private String from;
+    private String to;
+    List<Content> Contents;
+
+    public Conversation(String from, String to, List<Content> contents) {
+        this.from = from;
+        this.to = to;
+        Contents = contents;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public List<Content> getContents() {
+        return Contents;
+    }
+
+    public void setContents(List<Content> contents) {
+        Contents = contents;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+}
